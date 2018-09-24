c0520730f1441753cb6bf9d550133151fba2e567
Serhiy Hural <sergey.hural@gmail.com>
2018-09-24 10:43:14 +0300

    gdrp popup: links color; prod js/css

--- www/wp-content/themes/myfood/partials/gdrp-box.php
+++ www/wp-content/themes/myfood/partials/gdrp-box.php
@@ -2,7 +2,9 @@
     <div id="gdrp-popup" class="gdrp-popup">
         <div class=" gdrp-popup-container">
             <div class="gdrp-popup-title">
-                Myfood updated Privacy Policy. We use cookies to improve user experience with our site. By continuing to use the service, you have to agree to our use of cookies according to our Cookie Policy.
+                Myfood updated <a href="#">Privacy Policy</a>.
+                We use cookies to improve user experience with our site. By continuing to use the service, you have to agree to our use of cookies according to our
+                <a href="#">Cookie Policy</a>.
             </div>
 
             <a  class="gdrp-popup-button-close cbutton cbutton--border cbutton--border--green cbutton--dark cbutton--mask hv">
